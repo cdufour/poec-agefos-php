638b6ceeede85740d55bfd428cdb09b66b5acff1
chris <opusidea@gmail.com>
2017-11-02 15:38:11 +0100

    commit 16

--- module2/answer/manage.php
+++ module2/answer/manage.php
@@ -12,6 +12,17 @@ if (isset($_GET['id_question'])) {
     ':id_question' => intval($id_question)
   ));
   $title = $query->fetch(PDO::FETCH_OBJ)->title;
+
+  // récupération des réponses associées à la question traitée
+  $query2 = $db->prepare(
+    ' SELECT id, body, correct
+      FROM answer
+      WHERE id_question = :id_question
+    ');
+  $query2->execute(array(
+    ':id_question' => $id_question
+  ));
+  $answers = $query2->fetchAll(PDO::FETCH_OBJ);
 }
 
 if (isset($_POST['submit'])) {
@@ -24,26 +35,61 @@ if (isset($_POST['submit'])) {
     ' INSERT INTO answer (body, correct, id_question)
       VALUES (:body, :correct, :id_question)
     ');
-  $query->execute(array(
+  $result = $query->execute(array(
     ':body' => $_POST['body'],
     ':correct' => $correct,
     ':id_question' => intval($_POST['id_question'])
   ));
-}
 
+  ($result)
+    ? header('location:?route=answer/manage&id_question='.$_POST['id_question'])
+    : print('<p>L\'enregistrement de la réponse a échoué</p>');
+}
 ?>
 
 <h2>Question: <?=$title ?></h2>
-<h3>Ajouter une réponse</h3>
-<form method="POST">
-  <div class="form-group">
-    <label for="body">Texte</label>
-    <textarea name="body"></textarea>
+
+<div class="container">
+  <div class="row">
+    <div class="col-md-8">
+      <?php if(sizeof($answers) == 0): ?>
+        <p class="alert alert-warning">Aucune réponse enregistrée</p>
+      <?php else: ?>
+        <h3>Liste des réponses</h3>
+        <table class="table table-bordered table-striped">
+          <?php foreach($answers as $answer): ?>
+            <tr>
+              <td><?=$answer->body ?></td>
+              <td>
+                <?= ($answer->correct == 1) ? 'Bonne' : 'Mauvaise'; ?>
+                &nbsp;réponse
+              </td>
+              <td>
+                <a
+                  class="btn btn-danger btn-xs"
+                  href="?route=answer/delete&id=<?= $answer->id ?>">Supprimer</a>
+              </td>
+            </tr>
+          <?php endforeach ?>
+        </table>
+      <?php endif ?>
+    </div>
+    <div class="col-md-4">
+
+      <h3>Ajouter une réponse</h3>
+      <form method="POST" class="well">
+        <div class="form-group">
+          <label for="body">Texte</label>
+          <textarea name="body" required></textarea>
+        </div>
+        <div class="form-group">
+          <label for="correct">Bonne réponse</label>
+          <input type="checkbox" name="correct"/>
+        </div>
+        <input type="hidden" name="id_question" value="<?=$id_question?>">
+        <input type="submit" name="submit" value="Enregistrer">
+      </form>
+
+    </div>
   </div>
-  <div class="form-group">
-    <label for="correct">Bonne réponse</label>
-    <input type="checkbox" name="correct"/>
-  </div>
-  <input type="hidden" name="id_question" value="<?=$id_question?>">
-  <input type="submit" name="submit" value="Enregistrer">
-</form>
+</div>
--- module2/question/list.php
+++ module2/question/list.php
@@ -26,10 +26,10 @@ $questions = $query->fetchAll(PDO::FETCH_OBJ);
       <td>
         <a
           href="?route=answer/manage&id_question=<?= $question->id ?>"
-          class="btn btn-primary btn-xs">Gérer les réponses</a>
+          class="btn btn-default btn-xs">Gérer les réponses</a>
         <a
           href="?route=question/edit&id=<?= $question->id ?>"
-          class="btn btn-primary btn-xs">Modifier</a>
+          class="btn btn-default btn-xs">Modifier</a>
         <a
           href="?route=question/delete&id=<?= $question->id ?>"
           class="btn btn-danger btn-xs">Supprimer</a>
